b204db4dba8a23650e309f36012b6131571b578a
sim001 <simon.amoyal4@gmail.com>
2021-04-30 16:02:19 +0200

    img jabba

---
+++ src/app/core/models/party.ts
@@ -0,0 +1,13 @@
+/**
+ * partie du joueur
+ */
+ export class Party {
+    id: number;
+    date: Date;
+    duration: number;
+    nbTurns: number;
+    gainBefore: number;
+    gainAfter: number;
+
+  }
+  
